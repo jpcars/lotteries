ae7b01fdf2e6f51fcb07129153bec1efa85f128a
jpcars <jan.p.carstensen+git@gmail.com>
2023-03-29 23:04:58 +0200

    reintroduced errors instead of exceptions

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,11 @@ st.write(
 This is a small app that is supposed to compute useful quantities for different lottery procedures,
 which are commonplace in ethical debates surrounding the distribution of goods. So far this app is very
 minimalistic. Given a set of possibly overlapping groups it computes the probabilities that any particular
-group and any particular claimant will win the lottery. So far only Vong\'s exclusive composition sensitive
-lottery is implemented.
+group and any particular claimant will win the lottery. So far only Vong\'s exclusive and equal composition-sensitive
+lotteries are implemented.
 
 Further plans involve:
-- implementing other lotteries, such as Timmermann\'s individualist lottery and Vong\'s equal composition sensitive
-lottery, (if you want others, please say so)
+- implementing other lotteries, such as Timmermann\'s individualist lottery (if you want others, please say so)
 - implementing metrics to evaluate how well these lotteries fulfill certain potential ethical criteria,
 e.g. absolute fairness, comparative fairness (both in Vong\'s Paper) or procedural fairness (as described in Rasmussen)
 """
@@ -26,7 +25,7 @@ e.g. absolute fairness, comparative fairness (both in Vong\'s Paper) or procedur
 st.subheader("Glossary")
 st.write("EXCS - Vong's exclusive composition sensitive lottery")
 st.write("EQCS - Vong's equal composition sensitive lottery")
-st.write("IL - Timmermann's individualist lottery")
+st.write("TI - Timmermann's individualist lottery")
 
 st.subheader("Definition of the groups")
 with st.form("Number of groups"):
@@ -50,18 +49,20 @@ if "numGroups" in st.session_state.keys():
             )
         submitted = st.form_submit_button("Submit")
 
-if any([len(group) == 0 for group in groups]):
-    raise Exception("Please make sure that all groups are non-empty.")
+empty_groups = any([len(group) == 0 for group in groups])
+if empty_groups:
+    st.error("Please make sure that all groups are non-empty.")
 
 removed_duplicates = []
 for group in groups:
     if set(group) not in removed_duplicates:
         removed_duplicates.append(set(group))
 
-if len(removed_duplicates) < len(groups):
-    raise Exception("Please make sure that all groups are unique.")
+duplicates = len(removed_duplicates) < len(groups)
+if duplicates:
+    st.error("Please make sure that all groups are unique.")
 
-if submitted:
+if submitted and (not empty_groups) and (not duplicates):
     group_series = []
     claimant_series = []
     for LotteryCLass in [EXCSLottery, EQCSLottery]:
