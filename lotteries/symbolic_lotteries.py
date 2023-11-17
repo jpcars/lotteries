3b4869e9031af68b8840fad2fe8b7a17468244af
jpcars <jan.p.carstensen+git@gmail.com>
2023-11-17 07:49:07 +0100

    added store<->retrieve functionality

--- lotteries/symbolic_lotteries.py
+++ lotteries/symbolic_lotteries.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 from itertools import permutations
+from types import NoneType
+from typing import Optional, Union
 
 import pandas as pd
 import numpy as np
@@ -32,34 +34,41 @@ class Lottery:
         self.single_value_matrices = None
         self.canon_claimant_label = None
         self.canon_group_label = None
+        self.inverse_canon_claimant_label = None
+        self.inverse_canon_group_label = None
         self.nauty_certificate = None
         self.claims_mat = None
+        self.group_probabilities = None
+        self.nauty_graph = None
+        self.automorphisms = None
         self.supersets = defaultdict(set)
         self.subsets = defaultdict(set)
 
         self.compute_supersets()
         self.compute_useful_matrices()
+        self.construct_nauty_graph()
+        self.compute_autgrp()
         self.compute_canon_labels()
 
         self.base_claim = 1 / self.claimant_mat.shape[0]
 
-    def probabilities(self) -> (pd.Series, pd.Series):
-        """
-        TODO: adjust
-        Computes the probabilities that any particular group will win the lottery
-        :return: series of group probabilities
-        """
-        group_probabilities = {}
-        for group, properties in self.graph.edge_properties.items():
-            group_probabilities[group] = properties.claim
-        group_probabilities_series = pd.Series(
-            data=group_probabilities,
-            name=self.lottery_name
-            + ("_subgroups_removed" if self.remove_subgroups else ""),
-        )
-        group_probabilities_series.sort_index(inplace=True)
-        group_probabilities_series.index.name = "group"
-        return group_probabilities_series
+    # def probabilities(self) -> (pd.Series, pd.Series):
+    #     """
+    #     TODO: adjust
+    #     Computes the probabilities that any particular group will win the lottery
+    #     :return: series of group probabilities
+    #     """
+    #     group_probabilities = {}
+    #     for group, properties in self.graph.edge_properties.items():
+    #         group_probabilities[group] = properties.claim
+    #     group_probabilities_series = pd.Series(
+    #         data=group_probabilities,
+    #         name=self.lottery_name
+    #         + ("_subgroups_removed" if self.remove_subgroups else ""),
+    #     )
+    #     group_probabilities_series.sort_index(inplace=True)
+    #     group_probabilities_series.index.name = "group"
+    #     return group_probabilities_series
 
     def compute_useful_matrices(self):
         self.binary_membership_matrix = (~np.isclose(self.claimant_mat, 0)) * 1
@@ -69,7 +78,9 @@ class Lottery:
         inverse_indices.shape = self.claimant_mat.shape
         single_value_matrices = []
         symbolic_matrix = zeros(self.number_claimants, self.number_groups)
-        for i in range(len(self.unique_values)):
+        for i in range(
+            1, len(self.unique_values)
+        ):  # start at 1 bc 0 also counts as unique value, but we don't want it
             single_value_matrices.append((inverse_indices == i) * 1)
             symbolic_matrix = symbolic_matrix + Matrix(
                 (inverse_indices == i) * 1
@@ -77,7 +88,7 @@ class Lottery:
         self.single_value_matrices = single_value_matrices
         self.symbolic_matrix = symbolic_matrix
 
-    def compute_canon_labels(self):
+    def construct_nauty_graph(self):
         nauty_incidence_dict = {}
         number_rows = self.number_claimants
         number_cols = self.number_groups
@@ -94,32 +105,74 @@ class Lottery:
                 color.add(vertex_count)
                 vertex_count = vertex_count + 1
             vertex_coloring.append(color)
-        nauty_graph = pn.Graph(
+        self.nauty_graph = pn.Graph(
             number_of_vertices=vertex_count,
             adjacency_dict=nauty_incidence_dict,
             vertex_coloring=vertex_coloring,
         )
-        self.nauty_certificate = pn.certificate(nauty_graph)
-        canon_nauty_label = pn.canon_label(nauty_graph)
-        self.canon_claimant_label = canon_nauty_label[:number_rows]
+
+    def compute_canon_labels(self):
+        self.nauty_certificate = pn.certificate(self.nauty_graph)
+        canon_nauty_label = pn.canon_label(self.nauty_graph)
+        self.canon_claimant_label = canon_nauty_label[: self.number_claimants]
         self.canon_group_label = [
-            i - number_rows
-            for i in canon_nauty_label[number_rows : number_rows + number_cols]
+            i - self.number_claimants
+            for i in canon_nauty_label[
+                self.number_claimants : self.number_claimants + self.number_groups
+            ]
+        ]
+        self.inverse_canon_claimant_label = [
+            self.canon_claimant_label.index(i)
+            for i in range(len(self.canon_claimant_label))
+        ]
+        self.inverse_canon_group_label = [
+            self.canon_group_label.index(i) for i in range(len(self.canon_group_label))
         ]
 
-    def store_values(self):
-        """
-        If the certificate is not yet present, write a new entry, else do nothing
-        :return:
-        """
-        pass
+    def compute_autgrp(self):
+        generators, _, _, orbits, orbits_no = pn.autgrp(self.nauty_graph)
+        self.claimant_generators = [
+            generator[: self.number_claimants] for generator in generators
+        ]
+        self.claimant_generators = [
+            generator[
+                self.number_claimants : self.number_claimants + self.number_groups
+            ]
+            for generator in generators
+        ]
+        self.orbits = orbits[: self.number_claimants + self.number_groups]
+        self.automorphisms = pn.autgrp(self.nauty_graph)
 
-    def retrieve_values(self, lottery_name=None):
+    def store_values(self, prob_dict) -> None:
+        """
+        If the certificate is not yet present in dict, write a new entry, else do nothing
+        """
+        assert (
+            self.canon_group_label
+        ), "self.canon_group_label should be set at this point"
+        if self.nauty_certificate not in prob_dict:
+            prob_dict[self.nauty_certificate] = {
+                self.lottery_name: self.group_probabilities[self.canon_group_label]
+            }
+        elif self.lottery_name not in prob_dict[self.nauty_certificate]:
+            prob_dict[self.nauty_certificate][
+                self.lottery_name
+            ] = self.group_probabilities[self.canon_group_label]
+
+    def retrieve_values(self, prob_dict) -> Optional[np.array]:
         """
         Check whether there is an entry for the certificate. If so, retrieve results and do backwards translation
         :return:
         """
-        return False
+        print("check 0")
+        assert (
+            self.inverse_canon_group_label
+        ), "self.inverse_canon_group_label should be set at this point"
+        if (graph_dict := prob_dict.get(self.nauty_certificate)) is not None:
+            print("check 1")
+            if (probabilities := graph_dict.get(self.lottery_name)) is not None:
+                print("check 2")
+                return probabilities[self.inverse_canon_group_label]
 
     def compute_supersets(self):
         A = self.claimant_mat
@@ -148,10 +201,9 @@ class GroupBasedLottery(Lottery):
     def __init__(self, claimant_mat, remove_subgroups=False):
         super().__init__(claimant_mat, remove_subgroups)
 
-    def compute(self):
-        if self.retrieve_values(lottery_name=self.lottery_name):
-            # TODO: implement
-            pass
+    def compute(self, prob_dict) -> np.array:
+        if (probabilities := self.retrieve_values(prob_dict=prob_dict)) is not None:
+            return probabilities
         else:
             active_groups = set(range(self.number_groups))
             if (self.number_groups == 1) | (self.number_claimants == 1):
@@ -174,15 +226,21 @@ class GroupBasedLottery(Lottery):
                                 smaller_mat = smaller_mat[
                                     ~np.all(smaller_mat == 0, axis=1)
                                 ]
-                                probs_from_next_iteration = self.__class__(
+                                next_lottery_iteration = self.__class__(
                                     claimant_mat=smaller_mat
-                                ).compute()
+                                )
+                                probs_from_next_iteration = (
+                                    next_lottery_iteration.compute(prob_dict=prob_dict)
+                                )
+                                next_lottery_iteration = None
                                 probabilities[bigger_groups] = (
                                     probabilities[bigger_groups]
                                     + probabilities[group] * probs_from_next_iteration
                                 )
                                 probabilities[group] = 0
                             active_groups.remove(group)
+                self.group_probabilities = probabilities
+                self.store_values(prob_dict=prob_dict)
                 return probabilities
 
 
@@ -253,19 +311,98 @@ class EQCSLottery(GroupBasedLottery):
         Compute the non-iterated distributions of claims from the claimants to the groups
         """
         row_sums = self.binary_membership_matrix.sum(axis=1)
-        self.claims_mat = self.binary_membership_matrix / row_sums[:, np.newaxis] * self.base_claim
+        self.claims_mat = (
+            self.binary_membership_matrix / row_sums[:, np.newaxis] * self.base_claim
+        )
+
+
+# class ClaimantBasedLottery(Lottery):
+#     """
+#     Implements the general structure of an iterated claimant based lottery. Specific lotteries are implemented in
+#     subclasses.
+#     """
+#
+#     def __init__(self, claimant_mat, remove_subgroups=False):
+#         super().__init__(claimant_mat, remove_subgroups)
+#
+#     def compute(self):
+#         if self.retrieve_values():
+#             # TODO: implement
+#             pass
+#         else:
+#             active_groups = set(range(self.number_groups))
+#             if (self.number_groups == 1) | (self.number_claimants == 1):
+#                 return self.claims_mat.sum(axis=0)
+#             else:
+#                 probabilities = self.claims_mat.sum(axis=0)
+#                 while len(active_groups) > 0:
+#                     active_groups_copy = active_groups.copy()
+#                     for group in active_groups_copy:
+#                         if (
+#                             len(self.subsets[group].intersection(active_groups)) > 0
+#                         ):  # don't compute groups, which still have active subgroups
+#                             pass
+#                         else:
+#                             # if group has supersets, iterate on the lottery only on the supersets
+#                             # if it doesn't: don't do anything
+#                             bigger_groups = sorted(list(self.supersets[group]))
+#                             if len(bigger_groups) > 0:
+#                                 smaller_mat = self.claims_mat[:, bigger_groups]
+#                                 smaller_mat = smaller_mat[
+#                                     ~np.all(smaller_mat == 0, axis=1)
+#                                 ]
+#                                 probs_from_next_iteration = self.__class__(
+#                                     claimant_mat=smaller_mat
+#                                 ).compute()
+#                                 probabilities[bigger_groups] = (
+#                                     probabilities[bigger_groups]
+#                                     + probabilities[group] * probs_from_next_iteration
+#                                 )
+#                                 probabilities[group] = 0
+#                             active_groups.remove(group)
+#                 return probabilities
 
 
 if __name__ == "__main__":
-    claimant_mat = np.array(
-        [
-            [1, 0, 1, 1, 1],
-            [1, 0, 0, 0, 0],
-            [0, 1, 1, 1, 1],
-            [0, 1, 0, 0, 1],
-            [0, 0, 0, 1, 0],
-        ]
-    )
-    lottery = EQCSLottery(claimant_mat=claimant_mat)
-    probs = lottery.compute()
-    print(probs)
+    my_dict = {}
+    # my_array = np.array(
+    #     [
+    #         [1, 0, 1, 1, 1],
+    #         [1, 0, 0, 0, 0],
+    #         [0, 1, 1, 1, 1],
+    #         [0, 1, 0, 0, 1],
+    #         [0, 0, 0, 1, 0],
+    #     ]
+    # )
+    n_claimants = 6
+    ones = [1 for i in range(int(n_claimants / 2))]
+    zeroes = [0 for i in range(int(n_claimants / 2))]
+    my_array = np.transpose(np.array([ones + zeroes, zeroes + ones]))
+    for i in range(n_claimants):
+        for j in range(i + 1, n_claimants):
+            newcol = np.zeros(shape=(n_claimants, 1))
+            newcol[i] = 1
+            newcol[j] = 1
+            my_array = np.hstack([my_array, newcol])
+    lottery = EXCSLottery(claimant_mat=my_array)
+    print(my_array.shape)
+    generators, grpsize1, grpsize2, orbits, orbits_no = lottery.automorphisms
+    print(orbits[: my_array.shape[0] + my_array.shape[1]])
+
+    def to_cycles(perm):
+        pi = {i: p for i, p in enumerate(perm)}
+        cycles = []
+        while pi:
+            next_item = pi[next(iter(pi))]
+            cycle = []  # arbitrary starting element
+            while next_item in pi:
+                cycle.append(next_item)
+                next_item = pi[next_item]
+                del pi[cycle[-1]]
+            cycles.append(cycle)
+        return cycles
+
+    for generator in generators:
+        print([tuple(i) for i in to_cycles(generator) if len(i) > 1])
+    # probs = lottery.compute(my_dict)
+    # print(probs)
